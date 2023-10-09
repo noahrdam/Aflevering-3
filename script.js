e61c65b30b8dbe8e9b2d5a1cd57bbb3da12405f6
Noah Dam~ <noahrdam@gmail.com~>
2023-10-09 15:51:44 +0200

    så langt vi nåede

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("hello world");
-
 let contentElement = document.getElementById("0");
 
 console.log(contentElement);
@@ -10,24 +8,17 @@ console.log(mainContentElement);
 
 mainContentElement.removeChild(contentElement);
 
-/*
-for (let i = 0; i < 10; i++) {
-  let elementToInsert = contentElement.cloneNode(true);
-  elementToInsert.id = i;
-  mainContentElement.appendChild(elementToInsert);
-  console.log("element added with id: " + elementToInsert.id);
-}
-*/
+
 
 fetchContent("/albums.json").then((data) => {
   for (let i = 0; i < data.length; i++) {
     let elementToInsert = contentElement.cloneNode(true);
-    elementToInsert.id = i;
+    elementToInsert.id = data[i].genre;
 
     let albumNameToInsert = "<h1 class='title'>" + data[i].albumName + "</h1>";
     let albumcoverToInsert = "<img src='" + data[i].albumcover + "'>";
     let artistNameToInsert = "<p class='inner-content'>" + data[i].artistName + "</p>";
-    let genreToInsert = "<p class='inner-content'>" + data[i].genre + "</p>";
+    let genreToInsert = "<p class='inner-content genre'>" + data[i].genre + "</p>";
     let artistWebsiteToInsert = "<a href='"+data[i].artistWebsite+"'> <p class='inner-content'>" + data[i].artistWebsite + "</p> </a>";
     let productionYearToInsert = "<p class='inner-content'>" + data[i].productionYear + "</p>";
   
@@ -40,21 +31,7 @@ fetchContent("/albums.json").then((data) => {
     }
 trackListToInsert += "<div>";
 
-    let authorToInsert = "<p class='author'>" + data[i].author + "</p>";
-
-    if (data[i].author == "John Doe") {
-      authorToInsert = "<p class='author'>Annonymous</p>";
-    }
-
-    let contactToInsert =
-      "<p class='contact'>" +
-      "<a href='mailto:" +
-      data[i].email +
-      "'>" +
-      data[i].email +
-      "</a>" +
-      "</p>";
-
+    
     let htmlToInsert =
     albumNameToInsert + albumcoverToInsert + artistNameToInsert + genreToInsert + artistWebsiteToInsert + productionYearToInsert + trackListToInsert; 
 
@@ -65,6 +42,7 @@ trackListToInsert += "<div>";
     console.log("element added with id: " + elementToInsert.id);
   }
 });
+
 
 //Magi - det taler vi om senere!!
 async function fetchContent(url) {
