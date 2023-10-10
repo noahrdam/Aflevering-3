b57aa869932b70e7c389917a7f0b5b138961b554
kjuelsgaard <kristian@juelsgaard.dk>
2023-10-10 08:41:14 +0200

    Filtering virker

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let contentElement = document.getElementById("0");
+let contentElement = document.getElementsByClassName("content")[0];
 
 console.log(contentElement);
 
@@ -9,38 +9,48 @@ console.log(mainContentElement);
 mainContentElement.removeChild(contentElement);
 
 
-fetchContent("albums.json").then((data) => {
+
+fetchContent("/albums.json").then((data) => {
   for (let i = 0; i < data.length; i++) {
     let elementToInsert = contentElement.cloneNode(true);
-    elementToInsert.id = data[i].genre;
+    elementToInsert.className = "allAlbums " + data[i].genre;
 
     let albumNameToInsert = "<h1 class='title'>" + data[i].albumName + "</h1>";
     let albumcoverToInsert = "<img src='" + data[i].albumcover + "'>";
     let artistNameToInsert = "<p class='inner-content'>" + data[i].artistName + "</p>";
     let genreToInsert = "<p class='inner-content genre'>" + data[i].genre + "</p>";
-    let artistWebsiteToInsert = "<a href='"+data[i].artistWebsite+"' target='_blank'> <p class='inner-content'>" + data[i].artistWebsite + "</p> </a>";
+    let artistWebsiteToInsert = "<a href='" + data[i].artistWebsite + "'> <p class='inner-content'>" + data[i].artistWebsite + "</p> </a>";
     let productionYearToInsert = "<p class='inner-content'>" + data[i].productionYear + "</p>";
-  
-
     let trackListToInsert = "<div class = 'inner-content'>"
     for (let t = 0; t < data[i].trackList.length; t++) {
-      let trackNameToInsert = data[i].trackList[t].trackTitle; 
+      let trackNameToInsert = data[i].trackList[t].trackTitle;
       trackListToInsert += "<p>" + trackNameToInsert + "</p>";
 
     }
-trackListToInsert += "<div>";
+    trackListToInsert += "<div>";
 
-    
+
     let htmlToInsert =
-    albumNameToInsert + albumcoverToInsert + artistNameToInsert + genreToInsert + artistWebsiteToInsert + productionYearToInsert + trackListToInsert; 
+      albumNameToInsert + albumcoverToInsert + artistNameToInsert + genreToInsert + artistWebsiteToInsert + productionYearToInsert + trackListToInsert;
 
     elementToInsert.innerHTML = htmlToInsert;
 
+
     mainContentElement.appendChild(elementToInsert);
+
+
 
     console.log("element added with id: " + elementToInsert.id);
   }
 });
+
+function addCSSRule() {
+  const genre = document.getElementById('Genres').value;
+  const styleSheet = document.styleSheets[0];
+  styleSheet.insertRule('.allAlbums { display: none; }', styleSheet.cssRules.length);
+  styleSheet.insertRule('.' + genre + ' { display: block; }', styleSheet.cssRules.length);
+}
+
 
 
 //Magi - det taler vi om senere!!
@@ -49,3 +59,5 @@ async function fetchContent(url) {
   let json = await request.json();
   return json;
 }
+
+
